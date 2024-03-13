fb7e41ac03e243e399b420c3cd5a6738eedb5521
Akabim <abimanyu.ari.d.123@gmail.com>
2024-03-13 22:46:00 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#8c0c0b",
+        secondary: "#dab741",
+      },
+    },
+    fontFamily: {
+      heading: "Libre Baskerville, serif;",
+    },
+  },
+  plugins: [],
+};
